057e304ed1f620ebc9796ac2fb328c9ca7e1842a
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-21 23:33:43 +0700

    add CoreModels module

---
+++ core/Core.Models/ResponseModels/ResponseModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Core.Models.ResponseModels
+{
+    public class ResponseModel
+    {
+        public bool IsSucceed { get; set; }
+        public object Data { get; set; }
+        public string Message { get; set; }
+
+        public static ResponseModel Error =
+            new ResponseModel
+            {
+                Message = "Sever Error",
+                Data = new
+                {
+                    Contact = "nxgthanhcongcommunity@gmail.com"
+                }
+            };
+        public static ResponseModel Succeed(object data)
+        {
+            return new ResponseModel
+            {
+                IsSucceed = true,
+                Data = data,
+            };
+        }
+        public static ResponseModel Failed(string message, object data = null)
+        {
+            return new ResponseModel
+            {
+                IsSucceed = false,
+                Data = data,
+                Message = message
+            };
+        }
+    }
+}
