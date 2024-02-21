7d5a46bd22f69c3a01bc5a831ee43e2897018268
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-21 23:34:12 +0700

    add Core.Utilities module

---
+++ core/Core.Utilities/PasswordHasher.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Core.Utilities
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+                string hashedPassword = BitConverter.ToString(hashBytes).Replace("-", "");
+                return hashedPassword;
+            }
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            string hashedInputPassword = HashPassword(password);
+            return string.Equals(hashedInputPassword, hashedPassword);
+        }
+
+    }
+}
---
+++ core/Core.Utilities/ValidationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Mail;
+
+namespace Core.Utilities
+{
+    public static class ValidationHelper
+    {
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            try
+            {
+                // Use MailAddress class to validate email format
+                var addr = new MailAddress(email);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
