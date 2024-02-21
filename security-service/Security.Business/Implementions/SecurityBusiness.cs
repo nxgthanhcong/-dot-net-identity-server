5d828220789a5d822e8d685765e98fa52ed08274
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-21 23:36:17 +0700

    add Security.Business module

---
+++ security-service/Security.Business/Implementions/SecurityBusiness.cs
@@ -0,0 +1,30 @@
+﻿using Core.Models.ResponseModels;
+using Core.Utilities;
+using Security.Business.Interfaces;
+using Security.Models.RequestModels;
+using Security.Repositories.Interfaces;
+
+namespace Security.Business.Implementions
+{
+    public class SecurityBusiness : ISecurityBusiness
+    {
+        private readonly ISecurityRepository securityRepository;
+        public SecurityBusiness(ISecurityRepository securityRepository)
+        {
+            this.securityRepository = securityRepository;
+        }
+
+        public async Task<ResponseModel> Signup(ManualSignupReq manualSignupReq)
+        {
+            try
+            {
+                bool rs = await securityRepository.CreateUser(manualSignupReq);
+                return ResponseModel.Succeed(rs);
+            }
+            catch (Exception ex)
+            {
+                return ResponseModel.Error;
+            }
+        }
+    }
+}
---
+++ security-service/Security.Business/Interfaces/ISecurityBusiness.cs
@@ -0,0 +1,10 @@
+﻿using Core.Models.ResponseModels;
+using Security.Models.RequestModels;
+
+namespace Security.Business.Interfaces
+{
+    public interface ISecurityBusiness
+    {
+        Task<ResponseModel> Signup(ManualSignupReq manualSignupReq);
+    }
+}
