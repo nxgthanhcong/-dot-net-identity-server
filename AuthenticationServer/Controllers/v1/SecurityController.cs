90f024f06686e8506e6b12e3dba05c7bbba408c3
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-21 23:37:04 +0700

    add AuthenticationServer app

---
+++ AuthenticationServer/Controllers/v1/SecurityController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Security.Business.Interfaces;
+using Security.Models.RequestModels;
+
+namespace AuthenticationServer.Controllers.v1
+{
+    [Route("api/v1/security")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        private readonly ISecurityBusiness securityBusiness;
+        public SecurityController(ISecurityBusiness securityBusiness)
+        {
+            this.securityBusiness = securityBusiness;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> Signup(ManualSignupReq manualSignupReq)
+        {
+            return Ok(await securityBusiness.Signup(manualSignupReq));
+        }
+    }
+}
---
+++ AuthenticationServer/Infracture/ServiceRegisteration/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Core.DatabaseProviders.Implementions.Postgre;
+using Core.DatabaseProviders.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Security.Business.Implementions;
+using Security.Business.Interfaces;
+using Security.Repositories.Implementions;
+using Security.Repositories.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace AuthenticationServer.Infracture.ServiceRegisteration
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInternalServices(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddScoped<ISecurityRepository, SecurityRepository>();
+            services.AddScoped<ISecurityBusiness, SecurityBusiness>();
+            services.AddScoped<IDbProvider, PostgreProvider>();
+
+            services.Configure<PostgreConnectConfig>(configuration.GetSection("PostgreConnectConfig"));
+
+            return services;
+        }
+    }
+}
