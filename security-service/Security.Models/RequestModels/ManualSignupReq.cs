7c5054f035c37bc16224e1f40c4f48ad2b44b647
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-21 23:35:25 +0700

    add Security.Models module

---
+++ security-service/Security.Models/RequestModels/ManualSignupReq.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Security.Models.RequestModels
+{
+    public class ManualSignupReq
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+        
+        [Required]
+        [RegularExpression("^(?=.*[@])(?=.*[a-zA-Z]).{8,}$")]
+        public string Password { get; set; }
+
+    }
+}
