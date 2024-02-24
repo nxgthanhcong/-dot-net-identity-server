7c6c7e4e47bee5fd8b0f44d9b145f71d3041c5c8
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-24 12:29:50 +0700

    add TokenService

---
+++ security-service/Security.Services/Implementions/TokenService.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Security.Models.ProcessModels;
+using Security.Services.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Security.Services.Implementions
+{
+    public class TokenService : ITokenService
+    {
+        private readonly string RefreshKey;
+        private readonly string Issuer;
+        private readonly string Audience;
+        private readonly string Key;
+        public TokenService(IConfiguration configuration)
+        {
+            RefreshKey = configuration["TokenValidationParameters:RefreshKey"];
+            Issuer = configuration["TokenValidationParameters:ValidIssuer"];
+            Audience = configuration["TokenValidationParameters:ValidAudience"];
+            Key = configuration["TokenValidationParameters:Key"];
+        }
+
+        public string GenerateRefreshToken(UserModel user)
+        {
+            var claims = new[]
+            {
+                new Claim("username", user.Username),
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(RefreshKey));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(60), 
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateToken(UserModel user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("username", user.Username),
+                new Claim("role", user.Role ?? string.Empty)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Key));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: Issuer,
+                audience: Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(1), 
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public bool VerifyRefreshToken(string refreshToken, out ClaimsPrincipal claimsPrincipal)
+        {
+            claimsPrincipal = new ClaimsPrincipal();
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(RefreshKey);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+            };
+
+            SecurityToken validatedToken;
+            var principal = tokenHandler.ValidateToken(refreshToken, tokenValidationParameters, out validatedToken);
+
+            if (principal == null)
+            {
+                return false;
+            }
+
+            claimsPrincipal = principal; //.FindFirst("username")?.Value;
+            return true;
+        }
+    }
+}
---
+++ security-service/Security.Services/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using Security.Models.ProcessModels;
+using System.Security.Claims;
+
+namespace Security.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateToken(UserModel user);
+        string GenerateRefreshToken(UserModel user);
+        bool VerifyRefreshToken(string refreshToken, out ClaimsPrincipal claimsPrincipal);
+    }
+}
