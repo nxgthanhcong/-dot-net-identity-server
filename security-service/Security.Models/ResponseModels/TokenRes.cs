c5721a945ac04dcd43ce77c5b8e98ec7def55593
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-24 12:28:53 +0700

    add Token response model

---
+++ security-service/Security.Models/ResponseModels/TokenRes.cs
@@ -0,0 +1,9 @@
+﻿namespace Security.Models.ResponseModels
+{
+    public class TokenRes
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpireTime { get; set; }
+    }
+}
