0b7df0ae8f5a018e442749ee56770d97080135a1
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-25 12:14:43 +0700

    add httpService

---
+++ core/Core.HttpServices/Implementions/HttpService.cs
@@ -0,0 +1,21 @@
+﻿using Core.HttpServices.Interfaces;
+using System.Text.Json.Nodes;
+using System.Text;
+using System.Text.Json;
+
+namespace Core.HttpServices.Implementions
+{
+    public class HttpService : IHttpService
+    {
+        public async Task<HttpResponseMessage> PostAsync(string apiUrl, object contentObj)
+        {
+            string jsonBody = JsonSerializer.Serialize(contentObj);
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.PostAsync(apiUrl, new StringContent(jsonBody, Encoding.UTF8, "application/json"));
+
+                return response;
+            }
+        }
+    }
+}
---
+++ core/Core.HttpServices/Interfaces/IHttpService.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.HttpServices.Interfaces
+{
+    public interface IHttpService
+    {
+        Task<HttpResponseMessage> PostAsync(string url, object contentObj);
+    }
+}
