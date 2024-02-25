5575c10001a0693af58d3b81ccf88fa8a18b37ca
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-02-25 12:15:03 +0700

    add telegranNotifyService

---
+++ core/Core.NotifyService/Implementions/TelegramNotifyService.cs
@@ -0,0 +1,45 @@
+﻿using Core.HttpServices.Interfaces;
+using Core.Models.LoggingModels;
+using Core.NotifyService.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace Core.NotifyService.Implementions
+{
+    public class TelegramNotifyService : INotifyService
+    {
+        private readonly IHttpService httpService;
+        private readonly IConfiguration configuration;
+        private readonly string apiUrl;
+        public TelegramNotifyService(IHttpService httpService, IConfiguration configuration)
+        {
+            this.httpService = httpService;
+            this.configuration = configuration;
+
+            string botToken = configuration["TelegramNotifyBot:Token"];
+            apiUrl = $"https://api.telegram.org/bot{botToken}/sendMessage";
+        }
+
+        public async Task SendNotiMessageAsync(LoggingModel loggingModel)
+        {
+            string chatId = configuration["TelegramNotifyBot:ChatId"];
+
+            var responseAwait = httpService.PostAsync(apiUrl, new
+            {
+                chat_id = chatId,
+                text = JsonSerializer.Serialize(loggingModel)
+            });
+        }
+
+        public async Task SendNotiMessageAsync(string message)
+        {
+            string chatId = configuration["TelegramNotifyBot:ChatId"];
+
+            var responseAwait = httpService.PostAsync(apiUrl, new
+            {
+                chat_id = chatId,
+                text = JsonSerializer.Serialize(message)
+            });
+        }
+    }
+}
---
+++ core/Core.NotifyService/Interfaces/INotifyService.cs
@@ -0,0 +1,10 @@
+﻿using Core.Models.LoggingModels;
+
+namespace Core.NotifyService.Interfaces
+{
+    public interface INotifyService
+    {
+        Task SendNotiMessageAsync(LoggingModel loggingModel);
+        Task SendNotiMessageAsync(string message);
+    }
+}
